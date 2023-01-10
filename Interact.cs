49c16ecc30e56a1396f4320f060fbb10d0147057
danielniemiec44 <danielniemiec44@gmail.com>
2023-01-10 16:05:08 +0100

    a

--- Interact.cs
+++ Interact.cs
@@ -31,76 +31,6 @@ public class Interact : MonoBehaviour
         rp = new RenderParams(highlightMaterial);
 
         transform = firstPersonCamera.transform;
-
-        //highlightMesh = new Mesh();
-
-        //Vector3[] vertices = new Vector3[8];
-        //int[] triangles = new int[36];
-
-        /*
-        vertices[0] = new Vector3 (0, 0, 0);
-        vertices[1] = new Vector3 (1, 0, 0);
-        vertices[2] = new Vector3 (1, 1, 0);
-        vertices[3] = new Vector3 (0, 1, 0);
-        vertices[4] = new Vector3 (0, 1, 1);
-        vertices[5] = new Vector3 (1, 1, 1);
-        vertices[6] = new Vector3 (1, 0, 1);
-        vertices[7] = new Vector3 (0, 0, 1);
-
-
-        triangles[0] = 0;
-        triangles[1] = 2;
-        triangles[2] = 1;
-
-        triangles[3] = 0;
-        triangles[4] = 3;
-        triangles[5] = 2;
-
-        triangles[6] = 2;
-        triangles[7] = 3;
-        triangles[8] = 4;
-
-        triangles[9] = 2;
-        triangles[10] = 4;
-        triangles[11] = 5;
-
-        triangles[12] = 1;
-        triangles[13] = 2;
-        triangles[14] = 5;
-
-        triangles[15] = 1;
-        triangles[16] = 5;
-        triangles[17] = 6;
-
-        triangles[18] = 0;
-        triangles[19] = 7;
-        triangles[20] = 4;
-
-        triangles[21] = 0;
-        triangles[22] = 4;
-        triangles[23] = 3;
-        
-        triangles[24] = 5;
-        triangles[25] = 4;
-        triangles[26] = 7;
-
-        triangles[27] = 5;
-        triangles[28] = 7;
-        triangles[29] = 6;
-
-        triangles[30] = 0;
-        triangles[31] = 6;
-        triangles[32] = 7;
-
-        triangles[33] = 0;
-        triangles[34] = 1;
-        triangles[35] = 6;
-
-        highlightMesh.vertices = vertices;
-        highlightMesh.triangles = triangles;
-        highlightMesh.Optimize ();
-        highlightMesh.RecalculateNormals ();
-        */
     }
 
     // Update is called once per frame
@@ -131,7 +61,6 @@ public class Interact : MonoBehaviour
         Vector3 block = checkLookingAt();
         if(block.y >= 0) {
             WorldGenerator.removeBlock(block);
-            Debug.Log("Broken block: " + block);
         }
     }
 }
