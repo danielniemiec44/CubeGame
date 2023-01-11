b9c7a9fc1b8e5bac8f8341e235ee8376b78f4008
danielniemiec44 <danielniemiec44@gmail.com>
2023-01-11 13:23:11 +0100

    added new material

--- MaterialConstructor.cs
+++ MaterialConstructor.cs
@@ -7,17 +7,17 @@ using UnityEngine.Windows;
 public class MaterialConstructor : MonoBehaviour
 {
 public Material[] materials;
-public int pointerPosition = 0;
 
-public Texture2D texturePreview;
+public Texture2D[] texturePreview;
 
     void Start() {
-        materials = new Material[1];
-        //LoadImage("grass_block_side");
-        StartCoroutine(LoadTextureFromCache("grass_block"));
+        materials = new Material[2];
+        texturePreview = new Texture2D[2];
+        StartCoroutine(LoadTextureFromCache("grass_block", 0));
+        StartCoroutine(LoadTextureFromCache("dirt", 1));
     }
 
-    IEnumerator LoadTextureFromCache(string blockName) {
+    IEnumerator LoadTextureFromCache(string blockName, int index) {
         if(blockName == "grass_block") {
             var www1 = UnityWebRequestTexture.GetTexture("file:///C:/Users/danie/AppData/Roaming/.cubegame/block/" + blockName + "_side.png");
             var www2 = UnityWebRequestTexture.GetTexture("file:///C:/Users/danie/AppData/Roaming/.cubegame/block/" + blockName + "_top.png");
@@ -36,17 +36,6 @@ public Texture2D texturePreview;
                 Texture2D generatedTexture = new Texture2D(texture1.width * 4, texture1.height * 3, UnityEngine.Experimental.Rendering.DefaultFormat.LDR, UnityEngine.Experimental.Rendering.TextureCreationFlags.None);
                 generatedTexture.filterMode = FilterMode.Trilinear;
 
-                //Color greenColor = new Color(139, 187, 102, 1.0f);
-                
-                /*
-                int colorPointer = 0;
-                foreach(Color c in texture2.GetPixels()) {
-                    int x = colorPointer % 64;
-                    int y = (int) (colorPointer / 64);
-                    texture2Colored.SetPixel(x, y, Color.Lerp(c, color, 0));
-                    colorPointer++;
-                }
-                */
                 for(int x = 0; x < texture2.width; x++){
                     for(int y = 0; y < texture2.height; y++){
                         //texture2Colored.SetPixel(x, y, Color.Lerp(texture2.GetPixel(x, y), greenColor, Mathf.PingPong(Time.time, 1)));
@@ -59,30 +48,46 @@ public Texture2D texturePreview;
                     }
                 }
                 texture2Colored.Apply(false);
-                /*
-                for(int x = 0; x < generatedTexture.width; x++) {
-                    for(int y = 0; y < generatedTexture.height; y++) {
-                        generatedTexture.SetPixel(x, y, Color.blue);
-                    }
-                }
-                */
                 
                 Graphics.CopyTexture(texture1, 0, 0, 0, 0, texture1.width, texture1.height, generatedTexture, 0, 0, texture1.width, texture1.height * 2);
                 Graphics.CopyTexture(texture1Rotated, 0, 0, 0, 0, texture1Rotated.width, texture1Rotated.height, generatedTexture, 0, 0, 0, texture1Rotated.height);
                 Graphics.CopyTexture(texture1Rotated3, 0, 0, 0, 0, texture1Rotated3.width, texture1Rotated3.height, generatedTexture, 0, 0, texture1Rotated3.width, 0);
                 Graphics.CopyTexture(texture1Rotated2, 0, 0, 0, 0, texture1Rotated2.width, texture1Rotated2.height, generatedTexture, 0, 0, texture1Rotated2.width * 2, texture1Rotated2.height);
-                //
+                
                 Graphics.CopyTexture(texture2Colored, 0, 0, 0, 0, texture2Colored.width, texture2Colored.height, generatedTexture, 0, 0, texture2Colored.width, texture2Colored.height);
                 Graphics.CopyTexture(texture2Colored, 0, 0, 0, 0, texture2Colored.width, texture2Colored.height, generatedTexture, 0, 0, texture2Colored.width * 3, texture2Colored.height);
 
 
-                materials[pointerPosition] = new Material(Shader.Find("StandardCullOFF"));
-                //materials[pointerPosition].color = new Color(129, 184, 84, 0);
-                //materials[pointerPosition].mainTexture = Texture2D.blackTexture;
-                //materials[pointerPosition].SetColor("_Color", new Color(139,187,102,0f));
-                materials[pointerPosition].SetTexture("_MainTex", generatedTexture);
-                texturePreview = generatedTexture;
-                pointerPosition++;
+                materials[index] = new Material(Shader.Find("StandardCullOFF"));
+                materials[index].SetTexture("_MainTex", generatedTexture);
+                texturePreview[index] = generatedTexture;
+            }
+        } else {
+            var www1 = UnityWebRequestTexture.GetTexture("file:///C:/Users/danie/AppData/Roaming/.cubegame/block/" + blockName + ".png");
+   
+            yield return www1.SendWebRequest();
+            
+            if(www1.result == UnityWebRequest.Result.Success) {
+                Texture2D texture1 = DownloadHandlerTexture.GetContent(www1);
+                Texture2D texture1Rotated = rotateTexture(texture1, false);
+                Texture2D texture1Rotated2 = rotateTexture(texture1, true);
+                Texture2D texture1Rotated3 = rotateTexture(texture1Rotated, false);
+
+                Texture2D generatedTexture = new Texture2D(texture1.width * 4, texture1.height * 3, UnityEngine.Experimental.Rendering.DefaultFormat.LDR, UnityEngine.Experimental.Rendering.TextureCreationFlags.None);
+                generatedTexture.filterMode = FilterMode.Trilinear;
+                
+                Graphics.CopyTexture(texture1, 0, 0, 0, 0, texture1.width, texture1.height, generatedTexture, 0, 0, texture1.width, texture1.height * 2);
+                Graphics.CopyTexture(texture1Rotated, 0, 0, 0, 0, texture1Rotated.width, texture1Rotated.height, generatedTexture, 0, 0, 0, texture1Rotated.height);
+                Graphics.CopyTexture(texture1Rotated3, 0, 0, 0, 0, texture1Rotated3.width, texture1Rotated3.height, generatedTexture, 0, 0, texture1Rotated3.width, 0);
+                Graphics.CopyTexture(texture1Rotated2, 0, 0, 0, 0, texture1Rotated2.width, texture1Rotated2.height, generatedTexture, 0, 0, texture1Rotated2.width * 2, texture1Rotated2.height);
+                
+                Graphics.CopyTexture(texture1, 0, 0, 0, 0, texture1.width, texture1.height, generatedTexture, 0, 0, texture1.width, texture1.height);
+                Graphics.CopyTexture(texture1, 0, 0, 0, 0, texture1.width, texture1.height, generatedTexture, 0, 0, texture1.width * 3, texture1.height);
+
+
+                materials[index] = new Material(Shader.Find("StandardCullOFF"));
+                materials[index].SetTexture("_MainTex", generatedTexture);
+                texturePreview[index] = generatedTexture;
             }
         }
     }
